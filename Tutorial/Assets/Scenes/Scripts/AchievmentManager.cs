b34d070c5bfbb24e3d108b5812a660d3e0d9fda5
Raheel21 <moh13357822@students.solihull.ac.uk>
2019-12-06 22:46:38 +0000

    removed extra prefabs which were not needed

--- Tutorial/Assets/Scenes/Scripts/AchievmentManager.cs
+++ Tutorial/Assets/Scenes/Scripts/AchievmentManager.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class AchievmentManager : MonoBehaviour
 {
 
-    public GameObject achivementPrefab;  
-
+    public GameObject achivementPrefab;  //This is used to reference the achievment prefab
 
     // Start is called before the first frame update
     void Start()
     {
+        CreateAchievment("General"); //This will create a new achievment on the general category 
+
+
+
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
+
+    public void CreateAchievment(string category) //This will be a function to make a string category to put the achievment under and to find the parent
+    {
+        GameObject achievment = (GameObject)Instantiate(achivementPrefab); //This will be making a new achievment  
+        SetAchievmentInfo(category, achievment); 
+
+
+    }                     
+
+
+    public void SetAchievmentInfo(string category, GameObject achievment) 
+    {
+        achievment.transform.SetParent(GameObject.Find(category).transform); 
+
+        
+    }
+
+
+
+
 }
