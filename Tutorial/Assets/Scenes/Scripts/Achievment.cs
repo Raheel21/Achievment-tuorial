5966c885c0a22525c8832a8c08a4d7bbcd69e611
Raheel21 <moh13357822@students.solihull.ac.uk>
2020-01-11 16:54:28 +0000

    script change

---
+++ Tutorial/Assets/Scenes/Scripts/Achievment.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievment
+{
+    private string name;
+    
+    
+
+
+
+
+    private string description;
+
+    private bool unlocked;
+
+    private int points;
+
+    private int spriteIndex;
+
+    private GameObject achievmentRef; 
+
+    public Achievment(string name, string description, int points, int spriteIndex, GameObject achievmentRef)
+    {
+        this.name = name;
+        this.description = description;
+        this.unlocked = false;
+        this.points = points;
+        this.spriteIndex = spriteIndex;
+        this.achievmentRef = achievmentRef; 
+
+    }
+
+    public bool EarnAchievment()
+    {
+
+
+        if (!unlocked)
+        {
+
+            unlocked = true;
+            return true; 
+
+        }
+           return false; 
+    }
+}
--- Tutorial/Assets/Scenes/Scripts/AchievmentManager.cs
+++ Tutorial/Assets/Scenes/Scripts/AchievmentManager.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 using UnityEngine;
 
 
@@ -16,26 +16,18 @@ public class AchievmentManager : MonoBehaviour
 
     public GameObject achievmentMenu;
 
+    public GameObject visualAchievment;
+
+    public Dictionary<string, Achievment> achievments = new Dictionary<string, Achievment>(); 
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         activeButton = GameObject.Find("GeneralBtn").GetComponent<AchievmentButton>();
-        CreateAchievment("General", "TestTitle", "This is the description", 5, 0); //This will create a new achievment on the general category along with a title and description with a number value that represents points then another value that represents the sprite position      
-        CreateAchievment("General", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("General", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("General", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("General", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("General", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("General", "TestTitle", "This is the description", 5, 0);
-
-
-        CreateAchievment("Other", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("Other", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("Other", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("Other", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("Other", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("Other", "TestTitle", "This is the description", 5, 0);
-        CreateAchievment("Other", "TestTitle", "This is the description", 5, 0);
+        CreateAchievment("General", "Press W", "Press W to unlock this achievment", 5, 0); //This will create a new achievment on the general category along with a title and description with a number value that represents points then another value that represents the sprite position      
 
         foreach (GameObject achievmentList in GameObject.FindGameObjectsWithTag("AchievmentList"))
         {
@@ -57,26 +49,69 @@ public class AchievmentManager : MonoBehaviour
 
         }
 
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            EarnAchievment("Press W"); 
+        }
+
+
+
+
+
+
+
+
+
+
 
 
 
     }
 
+
+    public void EarnAchievment(string title)
+    {
+
+        if (achievments[title].EarnAchievment())
+        {
+            GameObject achievment = (GameObject)Instantiate(visualAchievment);
+            StartCoroutine(HideAchievment(achievment)); 
+        }
+    }
+
+    public IEnumerator HideAchievment(GameObject achievment)
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(achievment); 
+    }
+
+
+
+
+
+
+
     public void CreateAchievment(string category, string title, string description, int points, int spriteIndex) //This will be a function to make a string category to put the achievment under and to find the parent along with adding the title description and points for the achievment 
     {
         GameObject achievment = (GameObject)Instantiate(achivementPrefab); //This will be making a new achievment  
+
+        Achievment newAchievment = new Achievment(name, description, points, spriteIndex,achievment);
+
+        achievments.Add(title, newAchievment);   
+
         SetAchievmentInfo(category, achievment,title,description,points, spriteIndex); //For this im adding the category with the title description and points from the achievment info function 
 
 
     }                     
 
 
-    public void SetAchievmentInfo(string category, GameObject achievment, string title, string description, int points, int spriteIndex) //This is addeding the category as well as achievment game object with title description and points to this function
+    public void SetAchievmentInfo(string parent, GameObject achievment, string title) //This is addeding the category as well as achievment game object with title description and points to this function
     {
-       achievment.transform.SetParent(GameObject.Find(category).transform);//This will be setting the achievment to a set parent then it will be find a game object then put it in the category which will then set the transform of the category of the achievment  
+       achievment.transform.SetParent(GameObject.Find(parent).transform);//This will be setting the achievment to a set parent then it will be find a game object then put it in the category which will then set the transform of the category of the achievment  
        achievment.transform.localScale = new Vector3(1, 1, 1);//This is setting a new scale for my game object achievment
        achievment.transform.GetChild(0).GetComponent<Text>().text = title;//This is getting the child from the achievment along with the text component from unity for the child which is called title 
-       achievment.transform.GetChild(1).GetComponent<Text>().text = description;//This is getting the next child from the achievment gameobject along with the text component from unity for the child which is called description 
+       achievment.transform.GetChild(1).GetComponent<Text>().text = achievments[title].;//This is getting the next child from the achievment gameobject along with the text component from unity for the child which is called description 
        achievment.transform.GetChild(2).GetComponent<Text>().text = points.ToString();//This is getting the next child from the achievment gameobject along with the text component from unity and changing it to string as it will have a number value for the child which is called points
        achievment.transform.GetChild(3).GetComponent<Image>().sprite = sprites[spriteIndex];//This is getting the next child from the achievment gameobject along with the image component from unity and changing it to the sprite index which
     }
